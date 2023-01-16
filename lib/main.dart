c7a946b887e27aa89bc0009a9417ad37fa97ea69
SreedeviKS1705 <sreedevis1705@gmail.com>
2023-01-16 10:54:52 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -15,13 +16,36 @@ void main() {
   ));
 }
 
-class BallPage extends StatelessWidget {
+class BallPage extends StatefulWidget {
   const BallPage({Key? key}) : super(key: key);
 
   @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int buttonState = 1;
+
+  void setBallState(){
+    setState((){
+      buttonState=Random().nextInt(5)+1;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        height: double.infinity,
+        child: FlatButton(
+          child: Image(
+            image: AssetImage('images/ball$buttonState.png'),
+          ),
+          onPressed: () {
+            setBallState();
+          },
+        )
+    );
   }
 }
+
 
 
